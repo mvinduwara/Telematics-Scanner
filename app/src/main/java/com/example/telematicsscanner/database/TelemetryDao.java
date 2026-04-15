b74ceaca0a8110ad65e7e92400bb7ea5a52c2450
mvinduwara <mvinduwara@gmail.com>
2026-04-16 00:43:41 +0530

    Update TelemetryDao.java

--- app/src/main/java/com/example/telematicsscanner/database/TelemetryDao.java
+++ app/src/main/java/com/example/telematicsscanner/database/TelemetryDao.java
@@ -11,10 +11,12 @@ public interface TelemetryDao {
     @Insert
     void insert(TelemetryLog log);
 
-    // Pulls all data that hasn't been synced to the server yet
     @Query("SELECT * FROM telemetry_logs WHERE isSyncedWithCloud = 0")
     List<TelemetryLog> getUnsyncedLogs();
 
     @Query("UPDATE telemetry_logs SET isSyncedWithCloud = 1 WHERE id = :logId")
     void markAsSynced(int logId);
+
+    @Query("SELECT * FROM telemetry_logs ORDER BY timestamp ASC")
+    List<TelemetryLog> getAllLogsChronological();
 }
